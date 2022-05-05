2ddb5a3e91f84ec5df51852958521a23acaa386e
Marcelo Arias <hola@marceloarias.com>
2022-05-04 20:33:28 -0500

    Aplicación de Subject en PlatziWordle

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 /**
- * Aplicación de fromEvent en PlatziWordle:
- * fromEvent nos ayuda a atrapar eventos generados por elementos en el DOM.
+ * Aplicación de Subject en PlatziWordle:
+ * Subject nos ayuda a generar un observable donde podemos insertar valores fuera del observable.
+ * En PlatziWordle nos ayudará para alertar cuando el usuario ha ganado o ha perdido.
+ * En esta clase, creamos el mecanismo para alertar cuando el usuario ha ganado.
  */
 
-import { fromEvent } from "rxjs";
+import { fromEvent, Subject } from "rxjs";
+import WORDS_LIST from "./wordsList.json";
 
 const letterRows = document.getElementsByClassName("letter-row");
 const onKeyDown$ = fromEvent(document, "keydown");
 let letterIndex = 0;
 let letterRowIndex = 0;
+let userAnswer = [];
+const getRandomWord = () =>
+  WORDS_LIST[Math.floor(Math.random() * WORDS_LIST.length)];
+let rightWord = getRandomWord();
+console.log(`Right word: ${rightWord}`);
+
+const userWinOrLoose$ = new Subject();
 
 const insertLetter = {
   next: (event) => {
@@ -19,8 +29,31 @@ const insertLetter = {
       letterBox.textContent = pressedKey;
       letterBox.classList.add("filled-letter");
       letterIndex++;
+      userAnswer.push(pressedKey);
+    }
+  },
+};
+
+const checkWord = {
+  next: (event) => {
+    if (event.key === "Enter") {
+      // Si la respuesta del usuario es igual a la palabra correcta:
+      if (userAnswer.join("") === rightWord) {
+        // Emite un valor (vacío) hacia el observable `userWinOrLoose$` (ver línea 53)
+        userWinOrLoose$.next();
+      }
     }
   },
 };
 
 onKeyDown$.subscribe(insertLetter);
+onKeyDown$.subscribe(checkWord);
+
+// Cuando se emite un valor vacío, se ejecuta el siguiente observador:
+userWinOrLoose$.subscribe(() => {
+  let letterRowsWinned = letterRows[letterRowIndex];
+  // Lo siguiente nos permite pintar los contenedores de las letras con color verde 🟩 🟩 🟩 🟩 🟩
+  for (let i = 0; i < 5; i++) {
+    letterRowsWinned.children[i].classList.add("letter-green");
+  }
+});
